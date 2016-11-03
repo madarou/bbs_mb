a49acdf9733e63e1542b7b5cc0c4143fed94cf27
madarou <2016333261@qq.com>
2016-11-03 12:26:23 +0800

    add countip function, add argument

--- countip.py
+++ countip.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import re
 import sys, os, urllib2, json
+import argparse
 
 def get_ip_area(ip):  
     try:  
@@ -12,7 +13,7 @@ def get_ip_area(ip):
         if code == 0:   # success  
             #print(data['country_id']+' '+data['area']+' '+data['city']+' '+data['region'])
             #rt = data['country']+' '+data['area']+' '+data['city']+' '+data['region']+' '+data['isp']
-            rt =' '.join((data['country_id'],data['area'],data['city'],data['region'],data['isp']))
+            rt =' '.join((data['country'],data['area'],data['city'],data['region'],data['isp']))
             if rt.strip() == '':
                 rt = '解析地理位置失败'
         else:  
@@ -20,29 +21,40 @@ def get_ip_area(ip):
         request.close()
         return rt
     except Exception as ex:
-        request.close()
+        if request:
+            request.close()
         print(ex)
 
-def main():
-    f=open("C:\\Users\\ZYR\\Desktop\\bbs_ws.txt","r")
-    arr={} 
+def main(argv):
+    parser = argparse.ArgumentParser(description='统计ip访问次数及解析地理位置')
+    parser.add_argument('-d','--day',dest='day',action='store',help='eg: 2016-10-13',default='')
+    parser.add_argument('-f','--file',dest='file',action='store',help='被统计文件',default='C:\\Users\\ZYR\\Desktop\\bbs_ws.txt')
+    global args
+    args = parser.parse_args(argv[1:])
+    f=open(args.file,"r")
+    arr={}
+    ipaddress=re.compile(r'^\[INFO\]\s{1}\S+\s{1}\S+\s{1}(.*)={3}$')
+    if args.day!='':
+        ipaddress=re.compile(r'^\[INFO\]\s{1}\['+args.day+'\s{1}\S+\s{1}(.*)={3}$')
+        
     lines = f.readlines()
     for line in lines:
         line = line.decode('utf-8','ignore')
         #print line
-        ipaddress=re.compile(r'^\[INFO\]\s{1}\S+\s{1}\S+\s{1}(.*)={3}$')
+        #ipaddress=re.compile(r'^\[INFO\]\s{1}\S+\s{1}\S+\s{1}(.*)={3}$')
+        #ipaddress=re.compile(r'^\[INFO\]\s{1}\['+args.day+'\s{1}\S+\s{1}(.*)={3}$')
         match=ipaddress.match(line)
         if match:
+            print line
             ip = match.group(1) 
             if(arr.has_key(ip)): 
                 arr[ip]+=1
             else: 
-                arr[ip]=1 
+                arr.setdefault(ip,1)
     f.close()
     total=0
     print '====独立ip->访问次数===='
     for key,value in arr.items():
-        #print key+'('+get_ip_area(key)+')'+"->"+str(value)
         print ''.join((key,'-->',get_ip_area(key),'-->',str(value)))
         total=total+arr[key]
 
@@ -50,4 +62,5 @@ def main():
     print total
 
 if __name__ == '__main__':
-    main()
+    argv = sys.argv
+    main(argv)
