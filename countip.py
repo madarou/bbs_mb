19d0bc7dd28dbf96e416276dc1c16ce107b8fe91
madarou <2016333261@qq.com>
2016-11-03 11:46:58 +0800

    add countip function2

--- countip.py
+++ countip.py
@@ -5,15 +5,22 @@ import sys, os, urllib2, json
 def get_ip_area(ip):  
     try:  
         apiurl = "http://ip.taobao.com/service/getIpInfo.php?ip=%s" %ip  
-        content = urllib2.urlopen(apiurl).read()  
+        request = urllib2.urlopen(apiurl)
+        content = request.read()  
         data = json.loads(content)['data']  
         code = json.loads(content)['code']  
         if code == 0:   # success  
-            #print(data['country_id']+' '+data['area']+' '+data['city']+' '+data['region'])    
-            return data['country_id']+' '+data['area']+' '+data['city']+' '+data['region']
+            #print(data['country_id']+' '+data['area']+' '+data['city']+' '+data['region'])
+            #rt = data['country']+' '+data['area']+' '+data['city']+' '+data['region']+' '+data['isp']
+            rt =' '.join((data['country_id'],data['area'],data['city'],data['region'],data['isp']))
+            if rt.strip() == '':
+                rt = '解析地理位置失败'
         else:  
-            return '未知'  
-    except Exception as ex:  
+            rt = '未知'
+        request.close()
+        return rt
+    except Exception as ex:
+        request.close()
         print(ex)
 
 def main():
@@ -30,12 +37,13 @@ def main():
             if(arr.has_key(ip)): 
                 arr[ip]+=1
             else: 
-                arr.setdefault(ip,1) 
+                arr[ip]=1 
     f.close()
     total=0
     print '====独立ip->访问次数===='
-    for key in arr:
-        print key+'('+get_ip_area(key)+')'+"->"+str(arr[key])
+    for key,value in arr.items():
+        #print key+'('+get_ip_area(key)+')'+"->"+str(value)
+        print ''.join((key,'-->',get_ip_area(key),'-->',str(value)))
         total=total+arr[key]
 
     print '\n====总访问次数===='
